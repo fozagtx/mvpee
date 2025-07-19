02507c0573f20ea97a95c2585e34a83f697be92e
fawuzantech <ibrahimpima76@gmail.com>
2025-07-18 14:53:46 -1200

    new t3 start

--- app/api/gen/utils.ts
+++
@@ -1,6 +0,0 @@
-export function interpolatePrompt(
-	template: string,
-	variables: Record<string, string>,
-): string {
-	return template.replace(/\{(\w+)\}/g, (_, key) => variables[key] || "");
-}
--- app/auth/client.ts
+++
@@ -1,5 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-
-export const { signIn, useSession } = createAuthClient({
-	baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH,
-});
