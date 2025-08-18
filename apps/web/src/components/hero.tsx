1c2dd92f4bb2351862bdbb699f5b6745d432cbb1
devpima <ibrahimpima76@gmail.com>
2025-08-18 16:13:38 +0000

    removed the seo utils

--- apps/web/src/components/hero.tsx
+++ apps/web/src/components/hero.tsx
@@ -4,8 +4,8 @@ export default function Hero() {
   return (
     <div>
           <Header />
-      <div className="flex flex-row px-8 mt-[-32] items-center min-h-screen">
-<h1 className="text-4xl font-bold">Test the best models suitable for your MVP</h1>
+      <div className="flex flex-row px-8 items-center min-h-screen">
+      <h1 className="text-4xl font-bold">Test the best models suitable for your MVP</h1>
       </div>
     </div>
 
